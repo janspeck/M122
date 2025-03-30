59343dbb3e10a6554c15640cd0390a7f9cf6d237
janspeck <jan.speck@edu.gbssg.ch>
2025-03-30 09:43:50 +0200

    Create aufgabeB1.sh

---
+++ aufgabeB1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Programm: Quadrate-zeichnen.sh
+# Version:  1.1
+# Autor:    Jan Speck
+# Datum:    30.03.2025
+
+
+printf "\u2554\u2550\u2550\u2550\u2557\n"  
+printf "\u2551   \u2551\n"            
+printf "\u255A\u2550\u2550\u2550\u255D\n"  
+
+
+
+printf "        \u2554\u2550\u2550\u2550\u2557\n"
+printf "        \u2551   \u2551\n"
+printf "        \u255A\u2550\u2550\u2550\u255D\n"
